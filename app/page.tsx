08449ddc14bf8a49eb13438c09cbb32ce021e46b
Koala5617 <joaogchs5789@gmail.com>
2024-01-18 14:09:24 -0300

    Attribution added

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Block from './ui/block'
 import Profile from './ui/profile'
 import styles from './page.module.css'
 import { useState } from 'react'
+import Attribution from './ui/attribution'
 
 export default function Home() {
   const [timeFrame, setTimeFrame] = useState('weekly')
 
   return (
+    <>
     <main className={styles.wrapper}>
       <Profile setTimeFrame={setTimeFrame} timeFrame={timeFrame} />
       <Block dataIndex={0} title="Work" timeFrame={timeFrame} />
@@ -18,5 +20,7 @@ export default function Home() {
       <Block dataIndex={4} title='Social' timeFrame={timeFrame}/>
       <Block dataIndex={5} title='Self Care' timeFrame={timeFrame}/>
     </main>
+    <Attribution />
+    </>
   )
 }
---
+++ app/ui/attribution.tsx
@@ -0,0 +1,20 @@
+import Image from "@/node_modules/next/image"
+import styles from '../page.module.css'
+
+export default function Attribution() {
+    return (
+        <>
+        <footer id={styles.attribution} role="contentinfo">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            <a href="https://github.com/JoaoGabrielCHelena">
+                <Image
+                    src='/polygon.png'
+                    alt='My Github'
+                    width={56}
+                    height={56}
+                />
+            </a>
+        </footer>
+        </>
+    )
+}
