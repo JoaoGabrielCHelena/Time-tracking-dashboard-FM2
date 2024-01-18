f457fcef13ad8d936403ead646d6e50d9a79381b
Koala5617 <joaogchs5789@gmail.com>
2024-01-18 12:19:08 -0300

    Singular added

--- app/ui/block.tsx
+++ app/ui/block.tsx
@@ -3,15 +3,23 @@
 import styles from '../page.module.css'
 import data from "../data.json"
 
+
 export default function Block({ dataIndex, Category, title, timeFrame }: { dataIndex: number, Category: string, title: string, timeFrame: string }) {  
+    function pluralCheck(input:number) {
+        return input == 1 ? "hr" : "hrs"
+    }
+    const currentNum:number = data[dataIndex].timeframes[timeFrame as keyof object].current
+    const currentStr:string = `${currentNum}${pluralCheck(currentNum)}`
+    const previousNum:number = data[dataIndex].timeframes[timeFrame as keyof object].previous
+    const previousStr:string = `last week - ${previousNum}${pluralCheck(previousNum)}`
     return (
         <>
         <div id={`${styles[Category]}`} className={`${styles.category} ${styles.specialBlock}`}>
             <div className={styles.mainBlock}>
             <h1>{title}</h1>
             <img alt="options" src='./icon-ellipsis.svg' />
-            <div className={styles.hours}>{data[dataIndex].timeframes[timeFrame].current}hrs</div>
-            <div className={styles.previous}>last week - {data[dataIndex].timeframes[timeFrame].previous}hrs</div>
+            <div className={styles.hours}>{currentStr}</div>
+            <div className={styles.previous}>{previousStr}</div>
             </div>
         </div>
       </>
